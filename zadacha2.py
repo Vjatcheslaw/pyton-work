5fb5e739e0e36371ad40252a8c161e9592def7d2
Vjatcheslaw <kolotygin@gmail.com>
2024-11-08 16:35:41 +0700

    Add files via upload

---
+++ zadacha2.py
@@ -0,0 +1,15 @@
+from sys import float_info
+
+print(12+89-1)
+'1th pogram'
+print((9**0.5)*5)
+'2th program'
+print(9.99>9.9)
+print(1000 != 1000.1)
+'3th program'
+print(2*2+2)
+print(2*(2+2))
+print(2*2+2==(2+2)*2)
+'4th programm'
+print(float(123.456))
+print((int)(123.456*10))
